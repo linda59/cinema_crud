9522265451e1c3a7ac6ec603cace7f46fe2055e1
teambote <lou.yetu@laposte.net>
2016-12-14 16:04:44 +0100

    modif cinemactrl index

--- src/index.php
+++ src/index.php
@@ -15,16 +15,17 @@ use Semeformation\Mvc\Cinema_crud\controllers\HomeController;
 // on "assainit" les entrées
 session_start();
 
-$homeCtrl = new HomeController($logger);
+$homeCtrl   = new HomeController($logger);
+$cinemaCtrl = new CinemaController($logger);
 
 $sanitizedEntries = filter_input_array(INPUT_GET, ['action' => FILTER_SANITIZE_STRING]);
 
 if ($sanitizedEntries && $sanitizedEntries['action'] !== '') {
 // si l'action demandée est la liste des cinémas 
     switch ($sanitizedEntries['action']) {
+        // Activation de la route cinemasList
         case "cinemasList":
-// Activation de la route cinemasList
-            cinemasList($managers);
+            $cinemaCtrl->cinemasList($managers);
             break;
         case "createUser":
             $homeCtrl->createNewUser($managers);
@@ -48,13 +49,13 @@ if ($sanitizedEntries && $sanitizedEntries['action'] !== '') {
             deleteShowtime($managers);
             break;
         case "deleteCinema":
-            deleteCinema($managers);
+            $cinemaCtrl->deleteCinema($managers);
             break;
         case "deleteFavoriteMovie":
             deleteFavoriteMovie($managers);
             break;
         case "editCinema":
-            editCinema($managers);
+            $cinemaCtrl->editCinema($managers);
             break;
         case "editMovie":
             editMovie($managers);
