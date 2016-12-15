ea19fc8b967ab85bc49ef5310c8eb58f80c3a5db
samuel michaux <kameleon836@gmail.com>
2016-12-15 11:02:04 +0100

    Bug fixes #36
    Mise en place de la classe Router

--- src/Controllers/Router.php
+++ src/Controllers/Router.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Semeformation\Mvc\Cinema_crud\Controllers;
+
 use  Semeformation\Mvc\Cinema_crud\Controllers\CinemaController;
 use  Semeformation\Mvc\Cinema_crud\Controllers\FavoriteController;
 use  Semeformation\Mvc\Cinema_crud\Controllers\HomeController;
@@ -19,18 +20,73 @@ class Router{
     private $movieCtrl;
     private $showtimeCtrl;
 
-    public function routeRequest(){
-
-    }
-
     function __construct(LoggerInterface $logger){
         $this -> logger = $logger;
-        $this -> homeCtrl = $homeCtrl;
-        $this -> favoriteCtrl = $favoriteCtrl;
-        $this -> cinemaCtrl = $cinemaCtrl;
-        $this -> movieCtrl = $movieCtrl;
-        $this -> showtimeCtrl = $showtimeCtrl;
+        $this -> cinemaCtrl = new CinemaController($this -> logger);
+        $this -> homeCtrl = new HomeController($this -> logger);
+        $this -> favoriteCtrl = new FavoriteController($this -> logger);
+        $this -> showtimeCtrl = new ShowtimesController($this -> logger);
+        $this -> movieCtrl = new MovieController($this -> logger);
     }
+    public function routeRequest(){
+        $sanitizedEntries = filter_input_array(INPUT_GET, ['action' => FILTER_SANITIZE_STRING]);
+
+        if ($sanitizedEntries && $sanitizedEntries['action'] !== '') {
+            // si l'action demandée est la liste des cinémas
+            switch ($sanitizedEntries['action']) {
+                // Activation de la route cinemasList
+                case "cinemasList":
+                $this -> cinemaCtrl->cinemasList();
+                break;
+                case "createUser":
+                $this -> homeCtrl->createNewUser();
+                break;
+                case "cinemaShowtimes":
+                $this -> showtimeCtrl->cinemaShowtimes();
+                break;
+                case "moviesList":
+                $this -> movieCtrl->moviesList();
+                break;
+                case "movieShowtimes":
+                $this -> showtimeCtrl->movieShowtimes();
+                break;
+                case "editShowtime":
+                $this -> showtimeCtrl->editShowtime();
+                break;
+                case "deleteMovie":
+                $this -> movieCtrl->deleteMovie();
+                break;
+                case "deleteShowtime":
+                $this -> showtimeCtrl->deleteShowtime();
+                break;
+                case "deleteCinema":
+                $this -> cinemaCtrl->deleteCinema();
+                break;
+                case "deleteFavoriteMovie":
+                $this -> favoriteCtrl->deleteFavoriteMovie();
+                break;
+                case "editCinema":
+                $this -> cinemaCtrl->editCinema();
+                break;
+                case "editMovie":
+                $this -> movieCtrl->editMovie();
+                break;
+                case "editFavoriteMoviesList":
+                $this -> favoriteCtrl->editFavoriteMoviesList();
+                break;
+                case "editFavoriteMovie":
+                $this -> favoriteCtrl->editFavoriteMovie();
+                break;
+                default:
+                // Activation de la route par défaut (page d'accueil)
+                $this -> homeCtrl->home();
+            }
+        } else {
+            // Activation de la route par défaut (page d'accueil)
+            $this -> homeCtrl->home();
+        }
+    }
+
 
 
 
