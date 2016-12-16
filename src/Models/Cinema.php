1b11942e0449493f6b031bd2599037ea316b77d3
teambote <lou.yetu@laposte.net>
2016-12-16 11:11:23 +0100

    dao cinema
    classe Cinema getter setter

--- src/Models/Cinema.php
+++ src/Models/Cinema.php
@@ -10,90 +10,42 @@ namespace Semeformation\Mvc\Cinema_crud\Models;
 
 use Semeformation\Mvc\Cinema_crud\Includes\DAO;
 
+use Exception;
+
 /**
  * Description of Cinema
  *
  * @author admin
  */
-class Cinema extends DAO{
+class Cinema {
+    private $cinemaid;
+    private $denomination;
+    private $adresse;
     
-    
-    
-    
-    /**
-     * 
-     * @param type $denomination
-     * @param type $adresse
-     */
-    public function insertNewCinema($denomination, $adresse) {
-        // construction
-        $requete = "INSERT INTO cinema (denomination, adresse) VALUES ("
-                . ":denomination"
-                . ", :adresse)";
-        // exécution
-        $this->executeQuery($requete,
-                ['denomination' => $denomination,
-            'adresse' => $adresse]);
-        // log
-        if ($this->logger) {
-            $this->logger->info('Cinema ' . $denomination . ' successfully added.');
-        }
-    }
-    
-    /**
-     * 
-     * @param type $cinemaID
-     * @param type $denomination
-     * @param type $adresse
-     */
-    public function updateCinema($cinemaID, $denomination, $adresse) {
-        // on construit la requête d'insertion
-        $requete = "UPDATE cinema SET "
-                . "denomination = "
-                . "'" . $denomination . "'"
-                . ", adresse = "
-                . "'" . $adresse . "'"
-                . " WHERE cinemaID = "
-                . $cinemaID;
-        // exécution de la requête
-        $this->executeQuery($requete);
+    function getCinemaid() {
+        return $this->cinemaid;
     }
 
-    /**
-     * 
-     * @param type $cinemaID
-     */
-    public function deleteCinema($cinemaID) {
-        $this->executeQuery("DELETE FROM cinema WHERE cinemaID = "
-                . $cinemaID);
-
-        if ($this->logger) {
-            $this->logger->info('Cinema ' . $cinemaID . ' successfully deleted.');
-        }
+    function getDenomination() {
+        return $this->denomination;
     }
 
-    /**
-     * 
-     * @return type
-     */
-    public function getCinemasList() {
-        $requete = "SELECT * FROM cinema";
-        // on retourne le résultat
-        return $this->extraireNxN($requete);
+    function getAdresse() {
+        return $this->adresse;
     }
 
-    /**
-     * 
-     * @param type $cinemaID
-     * @return type
-     */
-    public function getCinemaInformationsByID($cinemaID) {
-        $requete = "SELECT * FROM cinema WHERE cinemaID = "
-                . $cinemaID;
-        $resultat = $this->extraire1xN($requete);
-        // on retourne le résultat extrait
-        return $resultat;
+    function setCinemaid($cinemaid) {
+        $this->cinemaid = $cinemaid;
     }
-    
+
+    function setDenomination($denomination) {
+        $this->denomination = $denomination;
+    }
+
+    function setAdresse($adresse) {
+        $this->adresse = $adresse;
+    }
+
+
     
 }
