6e232340e8dbe1f0ebe8e777252d782508d667a1
rplantefeve <romain.plantefeve@gmail.com>
2016-12-16 11:05:06 +0100

    Ajout de commentaires
    CinemaController

--- src/Controllers/CinemaController.php
+++ src/Controllers/CinemaController.php
@@ -6,141 +6,153 @@ use Semeformation\Mvc\Cinema_crud\Views\View;
 use Semeformation\Mvc\Cinema_crud\DAO\CinemaDAO;
 use Semeformation\Mvc\Cinema_crud\Models\Seance;
 use \Psr\Log\LoggerInterface;
+
 /**
  * Description of CinemaController
  *
- * @author admin
+ * @author teamDeformateur
  */
 class CinemaController {
+
+    /**
+     *
+     * @var CinemaDao 
+     */
     private $cinemasDAO;
+
+    /**
+     *
+     * @var Seance 
+     */
     private $seances;
 
-    public function __construct(LoggerInterface $logger=null) {
+    public function __construct(LoggerInterface $logger = null) {
         $this->cinemasDAO = new CinemaDAO();
-        $this->seances = new Seance($logger);
+        $this->seances    = new Seance($logger);
     }
 
     function cinemasList() {
 
-    $isUserAdmin = false;
+        $isUserAdmin = false;
 
-    if (array_key_exists("user", $_SESSION) and $_SESSION['user'] == 'admin@adm.adm') {
-        $isUserAdmin = true;
-    }
-    $cinemas = $this->cinemasDAO->getCinemasList();
-    $vue = new View('CinemasList');
-    $vue->generer((['isUserAdmin' => $isUserAdmin, 'cinemas' => $cinemas]));
+        if (array_key_exists("user", $_SESSION) and $_SESSION['user'] == 'admin@adm.adm') {
+            $isUserAdmin = true;
+        }
+        $cinemas = $this->cinemasDAO->getCinemasList();
+        $vue     = new View('CinemasList');
+        $vue->generer((['isUserAdmin' => $isUserAdmin, 'cinemas' => $cinemas]));
 //    require 'views/viewCinemasList.php';
-}
+    }
 
     function editCinema() {
-    //   session_start();
+        //   session_start();
 // si l'utilisateur n'est pas connecté ou sinon s'il n'est pas amdinistrateur
-    if (!array_key_exists("user", $_SESSION) or $_SESSION['user'] !== 'admin@adm.adm') {
-        // renvoi à la page d'accueil
-        header('Location: index.php');
-        exit;
-    }
+        if (!array_key_exists("user", $_SESSION) or $_SESSION['user'] !== 'admin@adm.adm') {
+            // renvoi à la page d'accueil
+            header('Location: index.php');
+            exit;
+        }
 
 // variable qui sert à conditionner l'affichage du formulaire
-    $isItACreation = false;
+        $isItACreation = false;
 
 // si la méthode de formulaire est la méthode POST
-    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "POST") {
+        if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "POST") {
 
-        // on "sainifie" les entrées
-        $sanEntries = filter_input_array(INPUT_POST,
-                ['backToList'             => FILTER_DEFAULT,
-            'cinemaID'               => FILTER_SANITIZE_NUMBER_INT,
-            'adresse'                => FILTER_SANITIZE_STRING,
-            'denomination'           => FILTER_SANITIZE_STRING,
-            'modificationInProgress' => FILTER_SANITIZE_STRING]);
+            // on "sainifie" les entrées
+            $sanEntries = filter_input_array(INPUT_POST,
+                    ['backToList'             => FILTER_DEFAULT,
+                'cinemaID'               => FILTER_SANITIZE_NUMBER_INT,
+                'adresse'                => FILTER_SANITIZE_STRING,
+                'denomination'           => FILTER_SANITIZE_STRING,
+                'modificationInProgress' => FILTER_SANITIZE_STRING]);
 
-        // si l'action demandée est retour en arrière
-        if ($sanEntries['backToList'] !== NULL) {
-            // on redirige vers la page des cinémas
-            // header('Location: cinemasList.php');
-            header('Location: index.php?action=cinemasList');
-            exit;
-        }
-        // sinon (l'action demandée est la sauvegarde d'un cinéma)
-        else {
-
-            // et que nous ne sommes pas en train de modifier un cinéma
-            if ($sanEntries['modificationInProgress'] == NULL) {
-                // on ajoute le cinéma
-                //$fctManager->insertNewCinema($sanEntries['denomination'], $sanEntries['adresse']);
-                //$fctCinema->insertNewCinema($sanEntries['denomination'], $sanEntries['adresse']);
-                $this->cinemasDAO->insertNewCinema($sanEntries['denomination'],
-                        $sanEntries['adresse']);
+            // si l'action demandée est retour en arrière
+            if ($sanEntries['backToList'] !== NULL) {
+                // on redirige vers la page des cinémas
+                // header('Location: cinemasList.php');
+                header('Location: index.php?action=cinemasList');
+                exit;
             }
-            // sinon, nous sommes dans le cas d'une modification
+            // sinon (l'action demandée est la sauvegarde d'un cinéma)
             else {
-                // mise à jour du cinéma
-                //$fctManager->updateCinema($sanEntries['cinemaID'], $sanEntries['denomination'], $sanEntries['adresse']);
-                //$fctCinema->updateCinema($sanEntries['cinemaID'], $sanEntries['denomination'], $sanEntries['adresse']);
-                $this->cinemasDAO->updateCinema($sanEntries['cinemaID'],
-                        $sanEntries['denomination'], $sanEntries['adresse']);
+
+                // et que nous ne sommes pas en train de modifier un cinéma
+                if ($sanEntries['modificationInProgress'] == NULL) {
+                    // on ajoute le cinéma
+                    //$fctManager->insertNewCinema($sanEntries['denomination'], $sanEntries['adresse']);
+                    //$fctCinema->insertNewCinema($sanEntries['denomination'], $sanEntries['adresse']);
+                    $this->cinemasDAO->insertNewCinema($sanEntries['denomination'],
+                            $sanEntries['adresse']);
+                }
+                // sinon, nous sommes dans le cas d'une modification
+                else {
+                    // mise à jour du cinéma
+                    //$fctManager->updateCinema($sanEntries['cinemaID'], $sanEntries['denomination'], $sanEntries['adresse']);
+                    //$fctCinema->updateCinema($sanEntries['cinemaID'], $sanEntries['denomination'], $sanEntries['adresse']);
+                    $this->cinemasDAO->updateCinema($sanEntries['cinemaID'],
+                            $sanEntries['denomination'], $sanEntries['adresse']);
+                }
+                // on revient à la liste des cinémas
+                //header('Location: cinemasList.php');.
+                header('Location: index.php?action=cinemasList');
+                exit;
             }
-            // on revient à la liste des cinémas
-            //header('Location: cinemasList.php');.
-            header('Location: index.php?action=cinemasList');
-            exit;
+        }// si la page est chargée avec $_GET
+        elseif (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "GET") {
+            // on "sainifie" les entrées
+            $sanEntries = filter_input_array(INPUT_GET,
+                    ['cinemaID' => FILTER_SANITIZE_NUMBER_INT]);
+            if ($sanEntries && $sanEntries['cinemaID'] !== NULL && $sanEntries['cinemaID'] !==
+                    '') {
+                // on récupère les informations manquantes
+                //$cinema = $fctManager->getCinemaInformationsByID($sanEntries['cinemaID']);
+                //$cinema = $fctCinema->getCinemaInformationsByID($sanEntries['cinemaID']);
+                $cinema = $this->cinemasDAO->getCinemaInformationsByID($sanEntries['cinemaID']);
+            }
+            // sinon, c'est une création
+            else {
+                $isItACreation = true;
+                $cinema        = [
+                    'CINEMAID'     => '',
+                    'DENOMINATION' => '',
+                    'ADRESSE'      => ''
+                ];
+            }
         }
-    }// si la page est chargée avec $_GET
-    elseif (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "GET") {
-        // on "sainifie" les entrées
-        $sanEntries = filter_input_array(INPUT_GET,
-                ['cinemaID' => FILTER_SANITIZE_NUMBER_INT]);
-        if ($sanEntries && $sanEntries['cinemaID'] !== NULL && $sanEntries['cinemaID'] !==
-                '') {
-            // on récupère les informations manquantes
-            //$cinema = $fctManager->getCinemaInformationsByID($sanEntries['cinemaID']);
-            //$cinema = $fctCinema->getCinemaInformationsByID($sanEntries['cinemaID']);
-            $cinema = $this->cinemasDAO->getCinemaInformationsByID($sanEntries['cinemaID']);
-        }
-        // sinon, c'est une création
-        else {
-            $isItACreation = true;
-            $cinema        = [
-                'CINEMAID'     => '',
-                'DENOMINATION' => '',
-                'ADRESSE'      => ''
-            ];
-        }
-    }
-    $vue = new View('EditCinema');
-    $vue->generer((['sanEntries'    => $sanEntries,
-        'cinema'        => $cinema,
-        'isItACreation' => $isItACreation]));
+        $vue = new View('EditCinema');
+        $vue->generer((['sanEntries'    => $sanEntries,
+            'cinema'        => $cinema,
+            'isItACreation' => $isItACreation]));
 //    require 'views/viewEditCinema.php';
-}
+    }
 
     function deleteCinema() {
-    // si l'utilisateur n'est pas connecté ou sinon s'il n'est pas amdinistrateur
-    if (!array_key_exists("user", $_SESSION) or $_SESSION['user'] !== 'admin@adm.adm') {
-        // renvoi à la page d'accueil
-        header('Location: index.php');
+        // si l'utilisateur n'est pas connecté ou sinon s'il n'est pas amdinistrateur
+        if (!array_key_exists("user", $_SESSION) or $_SESSION['user'] !== 'admin@adm.adm') {
+            // renvoi à la page d'accueil
+            header('Location: index.php');
+            exit;
+        }
+
+// si la méthode de formulaire est la méthode POST
+        if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "POST") {
+
+            // on "sainifie" les entrées
+            $sanitizedEntries = filter_input_array(INPUT_POST,
+                    ['cinemaID' => FILTER_SANITIZE_NUMBER_INT]);
+
+            // suppression de la préférence de film
+            //$fctManager->deleteCinema($sanitizedEntries['cinemaID']);
+            //$fctCinema->deleteCinema($sanitizedEntries['cinemaID']);
+
+            $this->seances->deleteShowtimeByIdCinema($sanitizedEntries['cinemaID']);
+            $this->cinemasDAO->deleteCinema($sanitizedEntries['cinemaID']);
+        }
+// redirection vers la liste des cinémas
+//header("Location: cinemasList.php");
+        header("Location: index.php?action=cinemasList");
         exit;
     }
 
-// si la méthode de formulaire est la méthode POST
-    if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === "POST") {
-
-        // on "sainifie" les entrées
-        $sanitizedEntries = filter_input_array(INPUT_POST,
-                ['cinemaID' => FILTER_SANITIZE_NUMBER_INT]);
-
-        // suppression de la préférence de film
-        //$fctManager->deleteCinema($sanitizedEntries['cinemaID']);
-        //$fctCinema->deleteCinema($sanitizedEntries['cinemaID']);
-
-        $this->seances->deleteShowtimeByIdCinema($sanitizedEntries['cinemaID']);
-        $this->cinemasDAO->deleteCinema($sanitizedEntries['cinemaID']);
-    }
-// redirection vers la liste des cinémas
-//header("Location: cinemasList.php");
-    header("Location: index.php?action=cinemasList");
-    exit;
-}
 }
